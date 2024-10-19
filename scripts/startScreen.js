88fcf59e1477db7bc603eaf7a1c4d4217123bea6
Joao <jm2matheus@gamil.com>
2024-10-19 10:21:02 -0300

    Ajustes leves

--- scripts/startScreen.js
+++ scripts/startScreen.js
@@ -41,27 +41,36 @@ function updateModalText() {
     case 1:
       modalText.innerHTML = `
         <h2>Bem-vindo ao Quiz do Zé Gotinha!</h2>
-        <p>Neste jogo de perguntas e respostas, você terá a chance de testar seus conhecimentos e aprender dicas valiosas para cuidar da <b>saúde</b> e <b>bem-estar</b>.</p>
-        <p>Cada pergunta traz informações importantes que podem ajudar a <b>prevenir doenças</b> e melhorar sua qualidade de vida. Aceite o desafio, jogue e descubra o quanto você sabe!</p>
-        <p>1/3</p>
+        <br/>
+        <p>Neste jogo de perguntas e respostas, você terá a chance de testar seus conhecimentos e aprender dicas valiosas para cuidar da <b class="modal-highlight">saúde</b> e <b class="modal-highlight">bem-estar</b>.</p>
+        <br/>
+        <p>Cada pergunta traz informações importantes que podem ajudar a <b class="modal-highlight">prevenir doenças</b> e melhorar sua qualidade de vida. Aceite o desafio, jogue e descubra o quanto você sabe!</p>
+        <br/>
+        <p class="modal-highlight">1/3</p>
       `;
       break;
     case 2:
       modalText.innerHTML = `
         <h2>Você não está sozinho nessa jornada!</h2>
-        <p>Durante o quiz, você terá <b>3 poderes especiais</b> para te ajudar a chegar mais longe:</p>
-        <p><b>Pular a Pergunta:</b> Não sabe a resposta? Use este poder para passar para a próxima.</p>
-        <p><b>Eliminar Alternativas:</b> Deixe o desafio um pouco mais fácil, removendo duas alternativas erradas.</p>
-        <p><b>Ajuda do Zé Gotinha:</b> Quando estiver em dúvida, peça ajuda do Zé Gotinha para dar uma dica.</p>
-        <p>2/3</p>
+        <br/>
+        <p>Durante o quiz, você terá <b class="modal-highlight">3 poderes especiais</b> para te ajudar a chegar mais longe:</p>
+        <br/>
+        <p><b class="modal-highlight">Pular a Pergunta:</b> Não sabe a resposta? Use este poder para passar para a próxima.</p>
+        <p><b class="modal-highlight">Eliminar Alternativas:</b> Deixe o desafio um pouco mais fácil, removendo duas alternativas erradas.</p>
+        <p><b class="modal-highlight">Ajuda do Zé Gotinha:</b> Quando estiver em dúvida, peça ajuda do Zé Gotinha para dar uma dica.</p>
+        <br/>
+        <p class="modal-highlight">2/3</p>
       `;
       break;
     case 3:
       modalText.innerHTML = `
         <h2>Realidade Aumentada</h2>
-        <p>Este quiz utiliza a incrível tecnologia de <b>Realidade Aumentada!</b> Basta apontar a câmera do seu celular para a imagem do seu cartão médico e as perguntas aparecerão na tela.</p>
-        <p>Caso a função não funcione corretamente, não se preocupe! Há um ícone de engrenagem no canto da tela onde você pode desativar a AR e continuar jogando normalmente. Divirta-se!</p>
-        <p>3/3</p>
+        <br/>
+        <p>Este quiz utiliza a incrível tecnologia de <b class="modal-highlight">Realidade Aumentada!</b> Basta apontar a câmera do seu celular para a <b class="modal-highlight">imagem do seu cartão médico</b> e as perguntas aparecerão na tela.</p>
+        <br/>
+        <p>Caso a função não funcione corretamente, não se preocupe! Há um ícone de engrenagem no canto da tela onde você pode <b class="modal-highlight">desativar</b> a AR e continuar jogando normalmente. Divirta-se!</p>
+        <br/>
+        <p class="modal-highlight">3/3</p>
       `;
       break;
     default:
